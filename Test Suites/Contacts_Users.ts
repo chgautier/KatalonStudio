1eb6aa120aa6f1e67808cba41fa7e17af2c7fb23
cgautier <cgautier@CNTR-PC-A009.int.centreon.com>
2018-03-05 17:54:47 +0100

    added test case and new test suite runs

--- Test Suites/Contacts_Users.ts
+++ Test Suites/Contacts_Users.ts
@@ -4,7 +4,7 @@
    <name>Contacts_Users</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-03-05T14:09:45</lastRun>
+   <lastRun>2018-03-05T15:31:36</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
