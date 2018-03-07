ca6a9be843aa2fb562bccae005554580fc58d89f
cgautier <cgautier@CNTR-PC-A009.int.centreon.com>
2018-03-07 10:47:50 +0100

    added changes tc

--- Test Suites/ACL.ts
+++ Test Suites/ACL.ts
@@ -4,10 +4,35 @@
    <name>ACL</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-03-06T16:08:18</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>e948394d-3dd5-48f7-886d-860b9049a874</testSuiteGuid>
+   <testCaseLink>
+      <guid>c0fe1ecd-4038-41f5-ab9f-037c8fd8cedc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Basic Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d61460e-b564-4d41-a2a0-d2d33d873259</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Access Group Create</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a9a2f371-6e6f-42d2-9312-ca4f36e5be4b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Non admin no split graphs menu access</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f19bccc-14cb-44fe-bb50-4b4f3d2df8ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Non admin split graphs menu access</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
